d313b2a97d566f532efbb13f2c0e2273578cfd2a
WongKang <13951085087@139.com>
2016-12-03 17:20:34 +0800

    Update IP address

--- start_service.py
+++ start_service.py
@@ -3,6 +3,17 @@
 import time,os,re,json
 from urllib2 import urlopen
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 # Update stored IP address with current IP address
 def updateStoredIP(filename):
 
@@ -21,6 +32,7 @@ def updateStoredIP(filename):
 	storedIP = IPFile.readline()
 	IPFile.close()
 
+
 	# Check the change
 	if storedIP.find(currentIP) < 0:
 
@@ -28,12 +40,14 @@ def updateStoredIP(filename):
 		IPFile = open(filename, 'w')
 		IPFile.write(currentIP)
 		IPFile.close()
-		print 'IP address has been udpated : '+currentIP
+		print bcolors.OKGREEN + 'IP address has been udpated : '+currentIP + bcolors.ENDC
 
 		# Commit to Github
 		os.system('git add '+filename)
 		os.system('git commit -m "Update IP address"')
 		os.system('git push -u origin master')
+	else:
+		print bcolors.OKGREEN + "." + bcolors.ENDC
 
 while 1:
 	updateStoredIP('IP.md')
